224426090209c2b5514706cb6a28290ec747b84d
Secret <haichuang221@163.com>
2017-07-17 00:49:01 +0800

    update protoc to 3.3.0 and fix compatibility

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
-FROM golang:1.8
+FROM golang:1.8.3
 
 RUN apt-get update && apt-get install -y git unzip
 
-RUN wget https://github.com/google/protobuf/releases/download/v3.1.0/protoc-3.1.0-linux-x86_64.zip \
-&& unzip protoc-3.1.0-linux-x86_64.zip -d protoc && rm -f protoc-3.1.0-linux-x86_64.zip
+RUN wget https://github.com/google/protobuf/releases/download/v3.3.0/protoc-3.3.0-linux-x86_64.zip \
+&& unzip protoc-3.3.0-linux-x86_64.zip -d protoc
 RUN cp protoc/bin/protoc ./bin/protoc
 
 RUN go get -u github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway
--- generate.sh
+++ generate.sh
@@ -3,9 +3,17 @@ GPRC_PROTO_PATH=/go/protoc/include
 protoc_dir=/protoc_dir
 
 echo "generate grpc"
-protoc --proto_path=$protoc_dir --proto_path=$GRPC_GATEWAY_PROTO_PATH --proto_path=$GPRC_PROTO_PATH \
-	--go_out=Mgoogle/api/annotations.proto=github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis/google/api,plugins=grpc:$protoc_dir $protoc_dir/*.proto
+protoc -I$protoc_dir \
+  -I$GOPATH/src \
+  -I$GRPC_GATEWAY_PROTO_PATH \
+  -I$GPRC_PROTO_PATH \
+  --go_out=plugins=grpc:$protoc_dir \
+  $protoc_dir/*.proto
 echo "generate gateway"
-protoc --proto_path=$protoc_dir --proto_path=$GRPC_GATEWAY_PROTO_PATH --proto_path=$GPRC_PROTO_PATH \
-	--grpc-gateway_out=logtostderr=true:$protoc_dir  $protoc_dir/*.proto
+protoc -I$protoc_dir \
+  -I$GOPATH/src \
+  -I$GRPC_GATEWAY_PROTO_PATH \
+  -I$GPRC_PROTO_PATH \
+  --grpc-gateway_out=logtostderr=true:$protoc_dir \
+  $protoc_dir/*.proto
 echo "finished"
